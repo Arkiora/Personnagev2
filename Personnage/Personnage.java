04570e0f1a32781109bd8830999d4da19868207a
Arkiora <sisakounmatthew@gmail.com>
2024-01-25 15:26:33 +0100

    Changement de classe Magicien à Gladiateur, changement des noms, ajout de random, ajout de commentaires, détermination gagnant

--- Personnage/Personnage.java
+++ Personnage/Personnage.java
@@ -1,29 +1,25 @@
 package Personnage;
+import java.util.Random;
 
 public class Personnage {
-	private String nom;
-	private int vie;
-	
-	public Personnage(String a, int b) {
-		this.nom = a;
-		this.vie = b;
-	}
-	
-	public String LeNom() {
-		return this.nom;
-	}
-	
-	public int LaVie() {
-		return this.vie;
-	}
-	
-	public void AugmenterVie(int a) {
-		this.vie += a;
-	}
-	
-	public void Attaque(Personnage a, int b) {
-		a.vie -= b/2;
-		this.vie -= b/2;
-	}
-}
+   private String nom;
+   private int vie;
 
+   public Personnage(String a, int b) {
+      this.nom = a;
+      this.vie = b;
+   }
+   public String LeNom() {
+      return this.nom;
+   }
+   public int LaVie() {
+      return this.vie;
+   }
+   public void Potion() {
+      this.vie += ((int) (Math.random() * ((50 - 10))));
+   }
+   public void Attaque(Personnage a, int b) {
+      a.vie -= b;
+      this.vie -= b/4;
+   }
+}
--- jeuPersonnage.java
+++ jeuPersonnage.java
@@ -1,22 +1,43 @@
 import Personnage.*;
+import java.util.Random;
+
 public class jeuPersonnage{
     public static void main(String args[]) {
-       Personnage UnSorcier = new Personnage("Sorcier",70);
+        //Ajout des rsonnages avec leurs noms et leur vie + déclaration de la vie des personnages //
+       Personnage UnSorcier = new Personnage("Ahmet", ((int) (Math.random() * ((75 - 15)))));
        System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
+       Personnage UnGladiateur = new Personnage("Matthew", ((int) (Math.random() * ((100 - 25)))));
+       System.out.println("La vie du gladiateur est " + UnGladiateur.LaVie());
+       System.out.println("");
 
-       Personnage UnMagicien = new Personnage("Magicien",100);
-       System.out.println("La vie du magicien est " + UnMagicien.LaVie());
-
+        //Print des noms défini pour les deux objets//
        System.out.println("Le nom du sorcier = " + UnSorcier.LeNom());
-       System.out.println("Le nom du magicien = " + UnMagicien.LeNom());
+       System.out.println("Le nom du gladiateur = " + UnGladiateur.LeNom());
+       System.out.println("");
 
-       System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
-       UnSorcier.AugmenterVie(10);
-       System.out.println("Apres la vie du sorcier est " + UnSorcier.LaVie());
+        //Utilisation d'une potion//
+       System.out.println("Le sorcier prend une potion");
+       UnSorcier.Potion();
+       System.out.println("La vie du du sorcier est maintenant de " + UnSorcier.LaVie());
+       System.out.println("");
 
-       System.out.println("le magicien attaque le sorcier ...");
-       UnMagicien.Attaque(UnSorcier,40);
-       System.out.println("Atak La vie du magicien est " + UnMagicien.LaVie());
-       System.out.println("Atak La vie du sorcier est " + UnSorcier.LaVie());
+        //Attaque d'un objet sur un autre//
+       System.out.println("Le gladiateur attaque le sorcier ...");
+       System.out.println("...");
+       System.out.println("...");
+       System.out.println(""); 
+       UnGladiateur.Attaque(UnSorcier,((int) (Math.random() * ((40 - 1)))));
+       
+       //Print de la vie finale//
+       System.out.println("Finalement, la vie du gladiateur est " + UnGladiateur.LaVie());
+       System.out.println("Finalement, la vie du sorcier est " + UnSorcier.LaVie());
+
+       //Détermination du gagnant//
+       if (UnGladiateur.LaVie() > UnSorcier.LaVie()) {
+        System.out.println("Le gagnant est le gladiateur");
+       }
+       else {
+        System.out.println("Le gagnant est le sorcier");
+       }
 	}
 }
