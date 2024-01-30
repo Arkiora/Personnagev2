a598f160530e86a150608b6d235911f84bebbad9
Arkiora <sisakounmatthew@gmail.com>
2024-01-30 09:51:34 +0100

    Ask for name at the start of the game

--- jeuPersonnage.java
+++ jeuPersonnage.java
@@ -1,12 +1,22 @@
 import Personnage.*;
 import java.util.Random;
+import java.util.Scanner;
 
 public class jeuPersonnage{
     public static void main(String args[]) {
-        //Ajout des rsonnages avec leurs noms et leur vie + déclaration de la vie des personnages //
-       Personnage UnSorcier = new Personnage("Ahmet", ((int) (Math.random() * ((75 - 15)))));
+        
+        //Demande des noms des personnages//
+        System.out.println("Veuillez saisir le nom du sorcier : ");
+        Scanner scan = new Scanner(System.in);
+        String nomSorcier = scan.nextLine();
+        System.out.println("Veuillez saisir le nom du gladiateur : ");
+        String nomGladiateur = scan.nextLine();
+        System.out.println("");
+
+        //Ajout des perrsonnages avec leurs noms et leur vie + déclaration de la vie des personnages //
+       Personnage UnSorcier = new Personnage(nomSorcier, ((int) (Math.random() * ((50 - 20)))));
        System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
-       Personnage UnGladiateur = new Personnage("Matthew", ((int) (Math.random() * ((100 - 25)))));
+       Personnage UnGladiateur = new Personnage(nomGladiateur, ((int) (Math.random() * ((70 - 40)))));
        System.out.println("La vie du gladiateur est " + UnGladiateur.LaVie());
        System.out.println("");
 
@@ -26,7 +36,7 @@ public class jeuPersonnage{
        System.out.println("...");
        System.out.println("...");
        System.out.println(""); 
-       UnGladiateur.Attaque(UnSorcier,((int) (Math.random() * ((40 - 1)))));
+       UnGladiateur.Attaque(UnSorcier,((int) (Math.random() * ((40 - 15)))));
        
        //Print de la vie finale//
        System.out.println("Finalement, la vie du gladiateur est " + UnGladiateur.LaVie());
@@ -34,10 +44,10 @@ public class jeuPersonnage{
 
        //Détermination du gagnant//
        if (UnGladiateur.LaVie() > UnSorcier.LaVie()) {
-        System.out.println("Le gagnant est le gladiateur");
+        System.out.println("Le gagnant est " + UnGladiateur.LeNom()) ; 
        }
        else {
-        System.out.println("Le gagnant est le sorcier");
+        System.out.println("Le gagnant est " + UnSorcier.LeNom());
        }
 	}
 }
